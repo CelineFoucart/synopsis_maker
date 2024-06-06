1a95e1cb11acc65610e5f73986413cffd60f3a2d
Céline Foucart <celinefoucart@yahoo.fr>
2024-06-06 20:07:24 +0200

    add: stats sur le tableau de bord

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,26 +2,44 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use App\Entity\Character;
+use App\Entity\Episode;
+use App\Entity\Place;
+use App\Entity\Synopsis;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        return $this->render('admin/dashboard.html.twig', [
+            'numberOfUsers' => $this->entityManager->getRepository(User::class)->count([]),
+            'numberOfSynopsis' => $this->entityManager->getRepository(Synopsis::class)->count([]),
+            'numberOfEpisodes' => $this->entityManager->getRepository(Episode::class)->count([]),
+            'numberOfCharacters' => $this->entityManager->getRepository(Character::class)->count([]),
+            'numberOfPlaces' => $this->entityManager->getRepository(Place::class)->count([]),
+            'numberOfArticles' => $this->entityManager->getRepository(Article::class)->count([])
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Administration');
+            ->setTitle('Administration')
+            ->setFaviconPath('assets/img/feather-bg-dark.png');
     }
 
     public function configureAssets(): Assets
